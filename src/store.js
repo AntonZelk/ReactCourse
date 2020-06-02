69a9b16311242f5f8b51fbac98022f501811eddf
AntonZelk <zelkdar@gmail.com>
2020-06-02 20:44:01 +0300

    lesson_11

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import reducer from "./reducers";
+import { createStore } from "redux";
+import { combineReducers } from "redux";
+import countReducer from "./reducers/count";
+
+const store = createStore(
+	combineReducers({ books: reducer, count: countReducer })
+);
+// const store = createStore(reducer);
+
+export default store;
